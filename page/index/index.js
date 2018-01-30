57a3d5e1bb3415452f2053385062659eb6c44bd4
李琨 <likun@likundeMacBook-Pro.local>
2018-01-30 22:35:26 +0800

    首页

---
+++ page/index/index.js
@@ -0,0 +1,86 @@
+// page/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners: [
+      {
+        id: 3,
+        img: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/index/banner_3.jpg',
+        url: '',
+        name: '百亿巨惠任你抢'
+      },
+      {
+        id: 1,
+        img: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/index/banner_1.jpg',
+        url: '',
+        name: '告别午高峰'
+      },
+      {
+        id: 2,
+        img: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/index/banner_2.jpg',
+        url: '',
+        name: '金牌好店'
+      }
+    ]
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
